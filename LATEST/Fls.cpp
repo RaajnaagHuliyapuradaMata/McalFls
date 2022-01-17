521b5dfe9d8c7d30fc0bcd8f0b3a62230905e70f
root <nagaraja.hm.0001@gmail.com>
2022-01-17 09:57:37 +0100

    Update: submodule Fls

--- LATEST/Fls.cpp
+++ LATEST/Fls.cpp
@@ -5,3 +5,33 @@ class_Fls Fls;
 FUNC(void, FLS_CODE) class_Fls::InitFunction(void){
 }
 
+FUNC(void, FLS_CODE) class_Fls::Erase(void){
+}
+
+FUNC(void, FLS_CODE) class_Fls::Write(void){
+}
+
+FUNC(void, FLS_CODE) class_Fls::Cancel(void){
+}
+
+FUNC(void, FLS_CODE) class_Fls::GetStatus(void){
+}
+
+FUNC(void, FLS_CODE) class_Fls::GetJobResult(void){
+}
+
+FUNC(void, FLS_CODE) class_Fls::Read(void){
+}
+
+FUNC(void, FLS_CODE) class_Fls::Compare(void){
+}
+
+FUNC(void, FLS_CODE) class_Fls::SetMode(void){
+}
+
+FUNC(void, FLS_CODE) class_Fls::GetVersionInfo(void){
+}
+
+FUNC(void, FLS_CODE) class_Fls::MainFunction(void){
+}
+
--- LATEST/Fls.h
+++ LATEST/Fls.h
@@ -5,7 +5,17 @@
 
 class class_Fls{
    public:
-      FUNC(void, FLS_CODE) InitFunction(void);
+      FUNC(void, FLS_CODE) InitFunction   (void);
+      FUNC(void, FLS_CODE) Erase          (void);
+      FUNC(void, FLS_CODE) Write          (void);
+      FUNC(void, FLS_CODE) Cancel         (void);
+      FUNC(void, FLS_CODE) GetStatus      (void);
+      FUNC(void, FLS_CODE) GetJobResult   (void);
+      FUNC(void, FLS_CODE) Read           (void);
+      FUNC(void, FLS_CODE) Compare        (void);
+      FUNC(void, FLS_CODE) SetMode        (void);
+      FUNC(void, FLS_CODE) GetVersionInfo (void);
+      FUNC(void, FLS_CODE) MainFunction   (void);
 };
 
 extern class_Fls Fls;
