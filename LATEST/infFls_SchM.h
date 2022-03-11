ddf4038a12868df9607992dcfe7d4faf83dcb97f
root <nagaraja.hm.0001@gmail.com>
2022-03-11 12:12:22 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Fls

--- LATEST/infFls_SchM.h
+++ LATEST/infFls_SchM.h
@@ -1,15 +1,13 @@
+#pragma once
 /*****************************************************/
-/* File   : Fls.cpp                                  */
+/* File   : infFls_SchM.h                            */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "module.h"
-#include "infFls_EcuM.h"
-#include "infFls_Dcm.h"
-#include "infFls_SchM.h"
+#include "Compiler_Cfg_Fls.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -22,15 +20,6 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
-class module_Fls:
-      public abstract_module
-{
-   public:
-      FUNC(void, FLS_CODE) InitFunction   (void);
-      FUNC(void, FLS_CODE) DeInitFunction (void);
-      FUNC(void, FLS_CODE) GetVersionInfo (void);
-      FUNC(void, FLS_CODE) MainFunction   (void);
-};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -43,51 +32,11 @@ class module_Fls:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Fls     Fls;
-infEcuMClient* gptrinfEcuMClient_Fls = &Fls;
-infDcmClient*  gptrinfDcmClient_Fls  = &Fls;
-infSchMClient* gptrinfSchMClient_Fls = &Fls;
+extern infSchMClient* gptrinfSchMClient_Fls;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, FLS_CODE) module_Fls::InitFunction(void){
-}
-
-FUNC(void, FLS_CODE) module_Fls::DeInitFunction(void){
-}
-
-FUNC(void, FLS_CODE) module_Fls::GetVersionInfo(void){
-}
-
-FUNC(void, FLS_CODE) module_Fls::MainFunction(void){
-}
-
-#include "Fls_Unused.h"
-
-FUNC(void, FLS_CODE) class_Fls_Unused::Erase(void){
-}
-
-FUNC(void, FLS_CODE) class_Fls_Unused::Write(void){
-}
-
-FUNC(void, FLS_CODE) class_Fls_Unused::Cancel(void){
-}
-
-FUNC(void, FLS_CODE) class_Fls_Unused::GetStatus(void){
-}
-
-FUNC(void, FLS_CODE) class_Fls_Unused::GetJobResult(void){
-}
-
-FUNC(void, FLS_CODE) class_Fls_Unused::Read(void){
-}
-
-FUNC(void, FLS_CODE) class_Fls_Unused::Compare(void){
-}
-
-FUNC(void, FLS_CODE) class_Fls_Unused::SetMode(void){
-}
 
 /*****************************************************/
 /* EOF                                               */
