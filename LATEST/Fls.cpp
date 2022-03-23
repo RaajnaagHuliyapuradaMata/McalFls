276ff29c909ebf6877838ec20dbeda0d5c273ad7
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:07:39 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Fls

--- LATEST/Fls.cpp
+++ LATEST/Fls.cpp
@@ -40,6 +40,16 @@ class module_Fls:
       FUNC(void, FLS_CODE) DeInitFunction (void);
       FUNC(void, FLS_CODE) GetVersionInfo (void);
       FUNC(void, FLS_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, FLS_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
