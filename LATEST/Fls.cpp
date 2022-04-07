4f0be5ee17f52fec70c983dc6f9eb7f13543772a
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:24:45 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Fls

--- LATEST/Fls.cpp
+++ LATEST/Fls.cpp
@@ -101,8 +101,8 @@ FUNC(void, FLS_CODE) module_Fls::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+            lptrCfg = &PBcfgFls;
          }
       }
       IsInitDone = E_OK;
