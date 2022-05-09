5d899ab424a33ecf459f77c24e2f28deeb85b6a8
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:35:25 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Fls

--- LATEST/Fls.cpp
+++ LATEST/Fls.cpp
@@ -35,6 +35,13 @@ class module_Fls:
       public abstract_module
    ,  public class_Fls_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Fls(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
