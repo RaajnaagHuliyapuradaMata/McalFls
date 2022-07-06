d5cd7499503b30c9b4fc0e7a8625c3c72c33e20d
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-06 17:30:22 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module for flash

--- LATEST/Fls.cpp
+++ LATEST/Fls.cpp
@@ -7,9 +7,7 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "Module.hpp"
-#include "CfgFls.hpp"
-#include "Fls_core.hpp"
-#include "infFls_Exp.hpp"
+#include "Fls.hpp"
 #include "infFls_Imp.hpp"
 
 /******************************************************************************/
@@ -32,26 +30,6 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-class module_Fls:
-      INTERFACES_EXPORTED_FLS
-      public abstract_module
-   ,  public class_Fls_Functionality
-{
-   private:
-/******************************************************************************/
-/* OBJECTS                                                                    */
-/******************************************************************************/
-
-   public:
-      FUNC(void, FLS_CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, FLS_CONFIG_DATA, FLS_APPL_CONST) lptrCfgModule
-      );
-      FUNC(void, FLS_CODE) DeInitFunction (void);
-      FUNC(void, FLS_CODE) MainFunction   (void);
-      FLS_CORE_FUNCTIONALITIES
-};
-
-extern VAR(module_Fls, FLS_VAR) Fls;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
