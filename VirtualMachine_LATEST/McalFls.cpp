93495810b778ab298a30a0ab0faf7ceaad644597
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:05:53 +0200

    Update: AUTOSAR basic software micro-controller abstraction driver module for flash

--- VirtualMachine_LATEST/McalFls.cpp
+++ VirtualMachine_LATEST/McalFls.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : McalFls.cpp                                                           */
+/* File   : McalFls.cpp                                                       */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define MCALFLS_AR_RELEASE_VERSION_MAJOR                                           4
-#define MCALFLS_AR_RELEASE_VERSION_MINOR                                           3
+#define MCALFLS_AR_RELEASE_VERSION_MAJOR                                       4
+#define MCALFLS_AR_RELEASE_VERSION_MINOR                                       3
 
 /******************************************************************************/
 /* MACROS                                                                     */
