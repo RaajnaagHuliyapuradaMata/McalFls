d799c9b6e48ca2965c3886d23929d1046b81de30
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:42 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module for flash

---
+++ InfineonTLE9879QXA40_R2023/infMcalFls_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infMcalFls_ServiceNvM.hpp                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTMCALFLS_AR_RELEASE_VERSION_MAJOR                                  4
+#define CONSTMCALFLS_AR_RELEASE_VERSION_MINOR                                  3
+
+#define Const_DefaultInit_ConstMcalFls                                                                           \
+   ,  {CONSTMCALFLS_AR_RELEASE_VERSION_MAJOR, CONSTMCALFLS_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
